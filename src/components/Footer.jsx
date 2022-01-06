3761949ff7b8e63e2b680eaaf0c6b32c4ef345df
Waleed Saad Mohamed Alkalla <87426985+Abohamssa@users.noreply.github.com>
2022-01-06 23:47:15 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const year = new Date().getFullYear();
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <p>Copyright © {year} </p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>Here goes the title</h1>
+            <p> Here goes the content</p>
+        </div>
+    );
+};
+
+export default Note;
